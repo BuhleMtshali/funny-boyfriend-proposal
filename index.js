dc1631f48a46e259032df9a3126179016da9aee0
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-07-17 22:28:48 +0200

    updated the index file

--- index.js
+++ index.js
@@ -12,22 +12,38 @@ function increaseFn() {
 }
 
 function agreeFunction() {
-  let newContent = `
+  setTimeout(function () {
+    let loader = `
+            <h1>Plase wait while I finalise our lives together....</h1>
+            <div>
+            <i class="fa-solid fa-spinner fa-spin" style="color: #000000;"></i>
+            </div>
+            <img src="https://media.giphy.com/media/q6RoNkLlFNjaw/giphy.gif?cid=ecf05e47e3m9b285xu1kr2ky2nbmvp4w13fscw2s2jz3zs0e&ep=v1_gifs_related&rid=giphy.gif&ct=g"/>
+`;
+
+    containerElement.innerHTML = loader;
+  }, 2000);
+
+  setTimeout(function () {
+    let newContent = `
                 <h1>Well well well...</h1>
                 <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWJvajZ4cXN6b29tOWhubWg2OTUyMGVqbG8yM29ianNneGlzeWhkNiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Dps6uX4XPOKeA/giphy.gif"/>
 `;
-  containerElement.innerHTML = newContent;
+    containerElement.innerHTML = newContent;
+  }, 9000);
+
+  //secondTimeoutFunction
   setTimeout(function () {
     let secondMsg = `
-<h1>Looks like<br/>
-<span class="red">YOU</span><br/>
-are all mine now...
-</h1>
-<img src="https://media.giphy.com/media/lY1F6BJjbRO3m/giphy.gif?cid=790b7611c2cwm1eqv0rhjjdodajq6dntbrkplr3lyhjdrfza&ep=v1_gifs_search&rid=giphy.gif&ct=g"/>
-`;
+                <h1>Looks like<br/>
+                <span class="red">YOU</span><br/>
+                are all mine now...
+                </h1>
+                <img src="https://media.giphy.com/media/lY1F6BJjbRO3m/giphy.gif?cid=790b7611c2cwm1eqv0rhjjdodajq6dntbrkplr3lyhjdrfza&ep=v1_gifs_search&rid=giphy.gif&ct=g"/>
+                `;
 
     containerElement.innerHTML = secondMsg;
-  }, 3000);
+  }, 12000);
 }
 
 noBtn.addEventListener("click", increaseFn);
